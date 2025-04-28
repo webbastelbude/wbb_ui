d39376393deb9b77bfc1e829f1da15f2bdcd20e4
webbastelbude <h.quick@webbastelbude.de>
2025-04-28 08:13:41 +0200

    Updated engine

--- lib/wbb_ui.rb
+++ lib/wbb_ui.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require_relative "wbb_ui/version"
 require_relative "wbb_ui/engine"
+require_relative "wbb_ui/ui"
 
 module WbbUi
   class Error < StandardError; end
--- lib/wbb_ui/engine.rb
+++ lib/wbb_ui/engine.rb
@@ -1,10 +1,35 @@
-# lib/wbb_ui/engine.rb
+require_relative "../../app/helpers/wbb_ui/ui_helper"
+
 module WbbUi
   class Engine < ::Rails::Engine
     isolate_namespace WbbUi
+    
+    initializer "wbb_ui.importmap", before: "importmap" do |app|
+      # NOTE: this will add pins from this engine to the main app
+      # https://github.com/rails/importmap-rails#composing-import-maps
+      app.config.importmap.paths << root.join("config/importmap.rb")
 
-    initializer "wbb_ui.assets.precompile" do |app|
-      app.config.assets.precompile += %w( wbb_ui_manifest.js )
+      # NOTE: something about cache; I did not look into it.
+      # https://github.com/rails/importmap-rails#sweeping-the-cache-in-development-and-test
+      app.config.importmap.cache_sweepers << root.join("app/assets/javascripts")
+    end
+
+    #initializer "wbb_ui.assets" do |app|
+    #  app.config.assets.paths << root.join("app/assets/javascripts/wbb_ui")
+    #  app.config.assets.precompile += %w( wbb_ui/controllers/*.js )
+    #end
+
+    initializer "wbb_ui.assets" do |app|
+      app.config.assets.paths << root.join("app/assets/javascripts/wbb_ui/controllers")
+    end
+
+    initializer "wbb_ui.load_helpers" do
+      ActiveSupport.on_load(:action_controller_base) do
+        helper WbbUi::UiHelper
+      end
+      ActiveSupport.on_load(:action_view_base) do
+        include WbbUi::UiHelper
+      end
     end
   end
 end
